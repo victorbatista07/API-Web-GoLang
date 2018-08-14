b1f7946bab96ba611660e6b4a3a2bbd61fe256bb
João Victor Rocha Batista <joaobatista@catho.com.br>
2018-08-14 17:10:33 -0300

    Changing hello message

--- controllers/usuarios.go
+++ controllers/usuarios.go
@@ -7,5 +7,5 @@ import (
 
 // Hello it's API's homepage
 func Hello (context echo.Context) error {
-	return context.String(http.StatusOK, "Olá")
+	return context.String(http.StatusOK, "Olá Mundo")
 }
